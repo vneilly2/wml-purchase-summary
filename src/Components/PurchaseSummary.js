9b6ac42f97035ad475d683adc544bc89928798c3
Vernon E. Neilly, II <28078313+vneilly2@users.noreply.github.com>
2019-01-19 01:38:09 -0800

    directory/file restructured + added 'PurchaseSummary.js'

---
+++ src/Components/PurchaseSummary.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class PurchaseSummary extends Component {
+  render() {
+    return (
+      <div className="purchase-summary-container">Inside PurchaseSummary</div>
+    );
+  }
+}
+
+export default PurchaseSummary;

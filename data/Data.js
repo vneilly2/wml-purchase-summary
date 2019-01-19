eb00d8f2cc557f4ea6fd2193e3c4c3e629525564
Vernon E. Neilly, II <28078313+vneilly2@users.noreply.github.com>
2019-01-19 10:15:45 -0800

    added data.js

---
+++ data/Data.js
@@ -0,0 +1,11 @@
+module.exports = {
+  products: [
+    {
+      name:
+        "OFM Essentials Racecar-Style Leather Gaming Chair, Multiple Colors",
+      price: 115.0,
+      colors: ["Black and Gray", "Black", "Blue"],
+      id: "793522810"
+    }
+  ]
+};

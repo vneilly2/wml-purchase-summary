ef1648c0b62ae1f43d95f6c7469cf65250941907
Vernon E. Neilly, II <28078313+vneilly2@users.noreply.github.com>
2019-01-19 01:53:28 -0800

    added Redux

--- src/Test/App.test.js
+++ src/Test/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { shallow } from "enzyme";
 import App from "../Components/App";
-import PurchaseSummary from "./PurchaseSummary";
+import PurchaseSummary from "../Components/PurchaseSummary";
 import { wrap } from "module";
 
 describe("App", () => {
